558259643c897e9d6b3d95d4f0039601cdcec4f3
RustamPython <121849951+RustamPython@users.noreply.github.com>
2023-04-12 11:00:02 +0300

    Calculator
    .exe file is 12288B or 12KB

---
+++ TheSmallestCPPcalculator.cpp
@@ -0,0 +1,2 @@
+﻿#include <iostream>
+using namespace std;int main(){int a,b;char z;for(;;){cin>>a;cin>>z;cin>>b;if(z=='+'){cout<<a+b;}if(z=='-'){cout<<a-b;}if(z=='*'){cout<<a*b;}if(z=='/'){cout<<a/b;}cout<<"\n\n";}}
